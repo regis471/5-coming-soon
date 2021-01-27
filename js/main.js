4c7c596814d3fa85e2edac3546abb4e9c15c1bf7
regis471 <76951483+regis471@users.noreply.github.com>
2021-01-27 12:57:36 +0200

    Render socials #16

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-console.log('coming soon');
+import {socialsData} from './data/socialData.js';
+import { renderSocials } from './components/renderSocials.js';
+
+renderSocials('footer > .socials', socialsData);
+
