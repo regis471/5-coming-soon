5cb4a54dcdf3581be1a8916b919d4147dcab1ac3
regis471 <76951483+regis471@users.noreply.github.com>
2021-02-04 11:36:45 +0200

    Progress bar begins #31 #32

---
+++ js/data/progressBarData.js
@@ -0,0 +1,16 @@
+const progressBarData = [
+    {
+        label: 'UX Design',
+        value : 90
+    },
+    {
+        label: 'Web Design',
+        value : 77
+    },
+    {
+        label: 'Web development',
+        value : 56
+    },
+];
+
+export { progressBarData }
