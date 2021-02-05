bbd733b0c1919c04406430616d69276a351fe20d
regis471 <76951483+regis471@users.noreply.github.com>
2021-02-05 16:21:26 +0200

    Progress bar validation #38

--- js/data/progressBarData.js
+++ js/data/progressBarData.js
@@ -1,7 +1,63 @@
 const progressBarData = [
+    123123,
+    'asasd',
+    true,
+    false,
+    null,
+    undefined,
+    
+    [],
+    {},
+    {
+        label : null
+    },
+    {
+        label : ''
+    },
+    {
+        label : 1542
+    },
+    {
+        label : 'Programming'
+    },
+    {
+        label : 'Programming',
+        value : null
+    },
+    {
+        label : 'Programming',
+        value : 'pewpew'
+    },
+    {
+        label : 'Programming',
+        value : Infinity
+    },
+    {
+        label : 'Programming',
+        value : NaN
+    },
+    // {
+    //     label : 'Programming',
+    //     value : 0
+    // },
+    {
+        label : '                          Programming         '
+    },
+    // {
+    //     label : 'Programming',
+    //     value : -0
+    // },
+    {
+        label : 'Programming',
+        value : 1100
+    },
+    // {
+    //     label : 'Programming',
+    //     value : 12.123123123123
+    // },
     {
         label: 'UX Design',
-        value : 91
+        value : 92
     },
     {
         label: 'Web Design',
