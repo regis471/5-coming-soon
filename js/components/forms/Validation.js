f67c4d6a7e6600b766d39e2a119728eface3988f
regis471 <76951483+regis471@users.noreply.github.com>
2021-02-10 08:10:26 +0200

    Validations #20

--- js/components/forms/Validation.js
+++ js/components/forms/Validation.js
@@ -22,6 +22,15 @@ class Validation {
         }
 
         //visos likusios - tik mazosios
+        if (Validation.isLowercaseButFirst(name)) {
+            return 'Vardo visos likusios raides, isskyrus pirma, turi buti mazosios.';
+        }
+
+        //sudaryta tik is raidziu (nekreipiant demesio i tikslias abeceles)
+        if (!Validation.onlyAlphabetLetters(name)) {
+            return ' Varde gali buti tik abeceles raides';
+        }
+
         //tik abeceles raides (galimybe nurodyti kokios abeceles  yra priimtinos)
         return true;
     }
@@ -35,10 +44,23 @@ class Validation {
             return 'Email priekyje ir gale neturi buti tarpo.';
         }
        //butinas tik vienas @ simbolis
+        if (!Validation.textContainsLetter(email, '@')) {
+            return 'El. pastas turi tureti viena @ simboli';
+        } 
+        const emailParts = email.split('@');
        //pries @ (lokali dalis) turi buti ne tuscias tekstas
+       // uztenka patikriniti jog pirmas simbolis nera @
+       if (!Validation.isNonEmptyText(emailParts[0])) {
+            return 'El. pasto lokali dalis turi buti ne tuscias';
+       }
+
        //uz @ (domeno dalis) turi buti ne tuscias tekstas
-       return true;
+       //uztenka patikrinti jog paskutinis simbolis nera @
+       if (!Validation.isNonEmptyText(emailParts[1])) {
+        return 'El. pasto domeno dalis turi buti ne tuscias';
     }
+       return true;
+}
     static isValidText(text) {
         //ne tuscias tekstas
         if (!Validation.isNonEmptyText(text)) {
@@ -66,6 +88,33 @@ class Validation {
 
     static noSpacesAroundText(text) {
         return text === text.trim();
+    }
+    static isLowercaseButFirst(text) {
+        const rest = text.slice(1);
+        return rest = rest.toLowercase();
+    }
+    static textContainsLetter(text, letter, count = 1) {
+        let letterCount = 0;
+        for (let symbol of text) {
+            if (symbol === letter) {
+                letterCount++;
+            }
+        }
+         
+        //aukstesnes funkcijos sutrumpinimas
+        // letterCount = text.split('').filter(symbol => symbol === letter);
+        return letterCount === count;
+    }
+    static onlyAlphabetLetters(text) {
+        const uppercase = text.toUpperCase();
+        const lowercase = text.toLowercase();
+        const size = text.length;
+
+        for (let i=0; i<size; i++) {
+            if (uppercase[i] === lowercase[i]) {
+                return false;
+            }
+        }
     }
 }
 export { Validation }
