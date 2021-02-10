284d9e149924605a925777cf9f64120b6927ed27
regis471 <76951483+regis471@users.noreply.github.com>
2021-02-10 15:19:45 +0200

    Lighthouse correction

--- js/components/renderSocials.js
+++ js/components/renderSocials.js
@@ -38,7 +38,7 @@ function renderSocials(selector, data) {
             continue;
         }
 
-        HTML += `<a href="${socialObject.href}" target="_blank" class="fa fa-${socialObject.icon}" ></a>`;
+        HTML += `<a href="${socialObject.href}" target="_blank" class="fa fa-${socialObject.icon}" >${socialObject.icon}</a>`;
     }
         //post logic validation
         if (!HTML === ''){
